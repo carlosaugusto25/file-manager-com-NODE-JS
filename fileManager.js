bef6848f7ee34920f3b2bb07d0d3c8395b80d512
Carlos Augusto <carlosmedeiros.dev@gmail.com>
2024-11-26 20:59:45 -0300

    criação do projeto

---
+++ fileManager.js
@@ -0,0 +1,84 @@
+//tipo de importação CommonJS
+const fs = require('fs') // file system
+const path = require('path')
+
+function createDirectory(dirPath){
+    return new Promise((resolve, reject) => { //função de callback
+        fs.mkdir(dirPath, {recursive: true}, (err) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(`Directory '${dirPath}' created successfully`)
+            }
+        })
+    })
+}
+
+function createFile(filePath, content = ''){
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, content, 'utf8', (err) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(`File '${filePath}' created successfully`)
+            }
+        })
+    })
+}
+
+function listFiles(dirPath){
+    return new Promise((resolve, reject) => {
+        fs.readdir(dirPath, (err, files) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(files)
+            }
+        })
+    })
+}
+
+function readFiles(filePath){
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        })
+    })
+}
+
+function writeFiles(filePath, content){
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, content, 'utf8', (err) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(`File '${filePath}' updated successfully`)
+            }
+        })
+    })
+}
+
+function deleteFiles(filePath){
+    return new Promise((resolve, reject) => {
+        fs.unlink(filePath, (err) => {
+            if(err){
+                reject(err)
+            }else{
+                resolve(`File '${filePath}' deleted successfully`)
+            }
+        })
+    })
+}
+
+module.exports = {
+    createDirectory,
+    createFile,
+    listFiles,
+    readFiles,
+    writeFiles,
+    deleteFiles
+}
---
+++ index.js
@@ -0,0 +1,70 @@
+const fileManager = require('./fileManager')
+const readlineSync = require('readline-sync');
+const path = require('path');
+
+async function main(){
+
+    const baseDir = path.join(__dirname, 'my_files');
+    fileManager.createDirectory(baseDir);
+
+    while(true){
+        console.log('\nMenu:');
+        console.log('1. Criar arquivo');
+        console.log('2. Listar arquivos');
+        console.log('3. Ler arquivo');
+        console.log('4. Escrever arquivo');
+        console.log('5. Deletar arquivo');
+        console.log('6. Sair');
+
+        const choice = readlineSync.question('Escolha uma opcao: ');
+
+        try {
+            switch(choice){
+                case '1':
+                    const fileName = readlineSync.question('Digite o nome do arquivo: ');
+                    const fileContent = readlineSync.question('Digite o conteudo do arquivo ou (deixe em branco): ');
+    
+                    const createFilePath = path.join(baseDir, fileName);
+                    const fileMessage = await fileManager.createFile(createFilePath, fileContent);
+    
+                    console.log(fileMessage);
+                    break;
+                case '2':
+                    const files = await fileManager.listFiles(baseDir);
+                    console.log('Arquivos no diretório:', files);
+                    break;
+                case '3':
+                    const readFileName = readlineSync.question('Digite o nome e extensão do arquivo: ');
+                    const readFilePath = path.join(baseDir, readFileName);
+                    const content = await fileManager.readFiles(readFilePath);
+                    console.log('Conteúdo do arquivo:', content);
+                    break;
+                case '4':
+                    const writeFileName = readlineSync.question('Digite o nome e extensão do arquivo: ');
+                    const writeFilePath = path.join(baseDir, writeFileName);
+                    const newContent = readlineSync.question('Digite o conteudo do arquivo: ');
+    
+                    const writeMessage = await fileManager.writeFiles(writeFilePath, newContent);
+    
+                    console.log(writeMessage);
+                    break;
+                case '5':
+                    const deleteFileName = readlineSync.question('Digite o nome e extensão do arquivo: ');
+                    const deleteFilePath = path.join(baseDir, deleteFileName);
+                    const deleteMessage = await fileManager.deleteFiles(deleteFilePath);
+                    console.log(deleteMessage);
+                    break;
+                case '6':
+                    console.log('Saindo...');
+                    return;
+                default:
+                    console.log('Opcao invalida');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+        
+    }
+}
+
+main();
